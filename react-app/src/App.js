8c7c626c3ee79da6430cc25adb16d12e10d18843
bum752 <bum752@gmail.com>
2018-11-01 09:52:05 +0900

    목록 출력(GET)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import axios from 'axios'
 
 import './App.css'
+import config from './config'
 
 import Navbar from './components/Navbar'
 import Board from './components/Board'
@@ -11,136 +13,23 @@ class App extends React.Component {
 
     this.state = {
       items: [[], [], []]
-      // items: {
-      //   0: {
-      //     0: [],
-      //     1: [],
-      //     2: [],
-      //     3: [],
-      //     4: []
-      //   },
-      //   1: {
-      //     0: [],
-      //     1: [],
-      //     2: [],
-      //     3: [],
-      //     4: []
-      //   },
-      //   2: {
-      //     0: [],
-      //     1: [],
-      //     2: [],
-      //     3: [],
-      //     4: []
-      //   }
-      // }
     }
   }
 
   componentDidMount() {
-    const data = [
-      {
-          "_id": {
-              "status": 2,
-              "priority": 4
-          },
-          "items": [
-              {
-                  "_id": "5bd955a78aa34354e06d3b97",
-                  "priority": 4,
-                  "title": "TEST TITLE",
-                  "content": "TEST CONTENT",
-                  "deadline": "2018-11-07T07:11:35.957Z",
-                  "status": 2,
-                  "__v": 0
-              },
-              {
-                  "_id": "5bd955a88aa34354e06d3b9b",
-                  "priority": 4,
-                  "title": "TEST TITLE 5",
-                  "content": "TEST CONTENT 5",
-                  "deadline": "2018-11-07T07:11:36.919Z",
-                  "status": 2,
-                  "__v": 0
-              }
-          ]
-      },
-      {
-          "_id": {
-              "status": 1,
-              "priority": 3
-          },
-          "items": [
-              {
-                  "_id": "5bd955a88aa34354e06d3b99",
-                  "priority": 3,
-                  "title": "TEST TITLE 4",
-                  "content": "TEST CONTENT 4",
-                  "deadline": "2018-11-07T07:11:36.452Z",
-                  "status": 1,
-                  "__v": 0
-              }
-          ]
-      },
-      {
-          "_id": {
-              "status": 0,
-              "priority": 2
-          },
-          "items": [
-              {
-                  "_id": "5bd955a78aa34354e06d3b96",
-                  "priority": 2,
-                  "title": "TEST TITLE",
-                  "content": "TEST CONTENT",
-                  "status": 0,
-                  "__v": 0
-              }
-          ]
-      },
-      {
-          "_id": {
-              "status": 0,
-              "priority": 3
-          },
-          "items": [
-              {
-                  "_id": "5bd955a88aa34354e06d3b9a",
-                  "priority": 3,
-                  "title": "TEST TITLE 5",
-                  "content": "TEST CONTENT 5",
-                  "deadline": "2018-11-07T07:11:36.689Z",
-                  "status": 0,
-                  "__v": 0
-              }
-          ]
-      },
-      {
-          "_id": {
-              "status": 0,
-              "priority": 4
-          },
-          "items": [
-              {
-                  "_id": "5bd955a88aa34354e06d3b98",
-                  "priority": 4,
-                  "title": "TEST TITLE 3",
-                  "content": "TEST CONTENT 3",
-                  "deadline": "2018-11-07T07:11:36.207Z",
-                  "status": 0,
-                  "__v": 0
-              }
-          ]
-      }
-    ]
+    axios.get(`${config.api}/items`)
+      .then(response => {
+        const { items } = this.state
 
-    const { items } = this.state
+        if (response.data){
+          response.data.forEach(item => {
+            items[item._id.status][item._id.priority] = item.items
+          })
 
-    data.forEach(item => {
-      items[item._id.status][item._id.priority] = item.items
-    })
+          this.setState({ items })
+        }
+      })
 
-    this.setState({ items })
   }
 
   render() {
