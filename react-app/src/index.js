00dfb21d1242baa0f9b6cd65839bb22b975ead3f
bum752 <bum752@gmail.com>
2018-11-03 16:24:28 +0900

    IE Support

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie9'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
